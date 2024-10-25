ee161439527a1109b4d86429979432cadd3e3608
Abel Tibeso <abeltibeso323@gmail.com>
2024-10-25 12:35:32 +0300

    main app entry point

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+
+<StrictMode>
+<Router>
+    <App />
+    
+ </Router>
+  </StrictMode>,
+)
